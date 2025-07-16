daf2a944f20c11884da6b9939d7506be9609b918
HieuPNT <tomnguyenhieu2004@gmail.com>
2025-07-16 17:00:47 +0700

    Adding doc comments to AuthInterface

--- app/Factory/Auth/AuthInterface.php
+++ app/Factory/Auth/AuthInterface.php
@@ -4,8 +4,26 @@ namespace App\Factory\Auth;
 
 interface AuthInterface
 {
+    /**
+     * Generate the OAuth authorization URL for the client to authenticate.
+     *
+     * @return string URL auth
+     */
     public function getOAuthUrl();
+
+    /**
+     * Exchange the authorization code for an access token from the OAuth provider.
+     *
+     * @param string $code The authorization code received from the OAuth provider
+     * @return string Access token
+     */
     public function getAccessToken(string $code);
 
+    /**
+     * Retrieve user information using the provided access token.
+     *
+     * @param string $accessToken The access token obtained from the OAuth provider
+     * @return array User information
+     */
     public function getUserInfo(string $accessToken);
 }
