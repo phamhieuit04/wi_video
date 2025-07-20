6bae0f1e5acea4729b1333ffdb7a1bcbd3d69889
HieuPNT <tomnguyenhieu2004@gmail.com>
2025-07-20 16:47:15 +0700

    Add home controller & update web.php

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Contracts\Repositories\UserRepositoryInterface;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    private $userRepo;
+
+    public function __construct(UserRepositoryInterface $userRepo)
+    {
+        $this->userRepo = $userRepo;
+    }
+
+    public function index(Request $request)
+    {
+        $user = $this->userRepo->getInfo(Auth::id());
+        return view('home', [
+            'user' => $user
+        ]);
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect('/auth');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn() => redirect('/auth'));
@@ -12,5 +13,7 @@ Route::group(['prefix' => 'auth'], function () {
 });
 
 Route::middleware(['auth'])->group(function () {
-    Route::get('/home', fn() => "<h1>Loggin thanh cong!</h1>");
+    Route::get('/home', [HomeController::class, 'index']);
+
+    Route::get('/logout', [HomeController::class, 'logout']);
 });
