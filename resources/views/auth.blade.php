c56fda1d9eb54cefe3af2f67cbc9d9f15072c947
HieuPNT <tomnguyenhieu2004@gmail.com>
2025-07-24 15:32:44 +0700

    Add dark mode support

--- resources/views/auth.blade.php
+++ resources/views/auth.blade.php
@@ -10,21 +10,21 @@
         />
         @vite('resources/css/app.css')
     </head>
-    <body class="bg-[#181818]">
+    <body class="bg-white dark:bg-[#181818]">
         <div
             class="container mx-auto flex h-screen items-center justify-center"
         >
             <div class="flex items-center gap-8">
                 <div class="flex w-[650px] flex-col gap-2 text-white">
                     <h1 class="text-8xl font-bold text-red-600">Wi Video</h1>
-                    <p class="text-xl">
+                    <p class="text-xl text-black dark:text-white">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit.
                         Unde dolores doloribus ipsa maiores velit eius alias
                         soluta, ad id non.
                     </p>
                 </div>
                 <div
-                    class="flex w-[500px] flex-col gap-12 rounded-2xl bg-white px-6 py-8"
+                    class="flex w-[500px] flex-col gap-12 rounded-2xl bg-white px-6 py-8 outline-2 outline-gray-300"
                 >
                     <h3 class="text-center text-4xl font-semibold">
                         Đăng nhập
@@ -32,21 +32,25 @@
                     <div class="flex flex-col gap-3">
                         <a
                             href="{{ url('/auth/facebook/redirect') }}"
-                            class="group flex items-center gap-2 rounded-xl px-4 py-4 outline-2 outline-blue-600 transition-all duration-200 hover:brightness-75"
+                            class="group flex items-center gap-2 rounded-xl px-4 py-4 outline-2 outline-gray-300 transition-all duration-200 hover:outline-black"
                         >
-                            <i
-                                class="fa-brands fa-facebook text-5xl text-blue-600 transition-all duration-200 group-hover:animate-bounce"
-                            ></i>
+                            <img
+                                src="{{ asset('assets/images/facebook.png') }}"
+                                alt=""
+                                class="h-13.5"
+                            />
                             <h1 class="text-lg">Tiếp tục với Facebook</h1>
                         </a>
                         <p class="text-center opacity-75">hoặc</p>
                         <a
-                            class="group flex items-center gap-2 rounded-xl px-4 py-4 outline-2 outline-green-500 transition-all duration-200 hover:brightness-75"
+                            class="group flex items-center gap-2 rounded-xl px-4 py-4 outline-2 outline-gray-300 transition-all duration-200 hover:outline-black"
                             href="{{ url('/auth/google/redirect') }}"
                         >
-                            <i
-                                class="fa-brands fa-google text-5xl text-green-600 transition-all duration-200 group-hover:animate-bounce"
-                            ></i>
+                            <img
+                                src="{{ asset('assets/images/google_ico.png') }}"
+                                alt=""
+                                class="h-13.5"
+                            />
                             <h1 class="text-lg">Tiếp tục với Google</h1>
                         </a>
                     </div>
