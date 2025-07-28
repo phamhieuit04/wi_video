7b3acfed270eec6645cf5353b74ef141f4676199
HieuPNT <tomnguyenhieu2004@gmail.com>
2025-07-28 22:00:14 +0700

    Add upload & report video modals

---
+++ resources/js/Modals/ReportVideoModal.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="fixed inset-0 bg-black/75 flex items-center justify-center">
+        <div class="bg-white text-black p-5 w-[450px] flex flex-col gap-5 rounded-xl">
+            <div class="flex items-center justify-between">
+                <h5 class="font-bold text-3xl">
+                    Báo cáo vi phạm
+                </h5>
+                <button type="button" class="hover:opacity-75 cursor-pointer text-2xl" @click="$emit('close_modal')">
+                    <i class="fa-solid fa-xmark"></i>
+                </button>
+            </div>
+            <div class="flex flex-col gap-1">
+                <label>
+                    Nội dung báo cáo (*)
+                </label>
+                <textarea class="w-full rounded-lg outline-2 outline-gray-300 p-2 min-h-20" maxlength="255"></textarea>
+            </div>
+            <div class="flex gap-3 justify-end">
+                <button @click="$emit('close_modal')" type="button"
+                    class="cursor-pointer hover:opacity-75">Đóng</button>
+                <button type="button"
+                    class="bg-red-600 text-white py-2 px-4 rounded-lg hover:opacity-75 cursor-pointer">Gửi báo
+                    cáo</button>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ resources/js/Modals/UploadVideoModal.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="fixed inset-0 bg-black/75 flex items-center justify-center">
+        <div class="bg-white text-black p-5 rounded-xl w-[450px] flex flex-col gap-5">
+            <h5 class="text-center font-bold text-3xl">Đăng tải video</h5>
+            <div class="flex flex-col gap-2">
+                <div class="flex justify-between items-center gap-2">
+                    <label for="choose_video"
+                        class="bg-red-600 text-white py-2 px-4 rounded-md hover:opacity-75 cursor-pointer min-w-32 w-32 text-center">
+                        Chọn video
+                    </label>
+                    <p class="text-lg truncate">Videonameeeeeeeeeeeeeeeeeeeeeeeeee.mp4</p>
+                </div>
+                <input id="choose_video" type="file" accept=".mp4" value="" class="hidden" />
+                <div class="flex flex-col gap-1">
+                    <p>Mô tả:</p>
+                    <textarea class="w-full rounded-lg outline-2 outline-gray-300 p-2 min-h-20"
+                        maxlength="255"></textarea>
+                </div>
+            </div>
+            <div class="flex gap-3 justify-end">
+                <button @click="$emit('close_modal')" type="button"
+                    class="cursor-pointer hover:opacity-75">Đóng</button>
+                <button type="button"
+                    class="bg-red-600 text-white py-2 px-4 rounded-lg hover:opacity-75 cursor-pointer">Đăng tải</button>
+            </div>
+        </div>
+    </div>
+</template>
