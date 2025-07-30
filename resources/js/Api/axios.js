e55f309f9907857bec9e55224a675b9b364f8c98
HieuPNT <tomnguyenhieu2004@gmail.com>
2025-07-30 20:39:06 +0700

    Update axios.js

--- resources/js/Api/axios.js
+++ resources/js/Api/axios.js
@@ -4,9 +4,6 @@ const API_BASE_URL = 'http://localhost:8000/api';
 
 const api = axios.create({
     baseURL: API_BASE_URL,
-    headers: {
-        'Content-Type': 'application/json',
-    },
 });
 
 export default api;
