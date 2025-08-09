7e8202b15a14c2e8bbba7e5f1eb88df4df252ba9
HieuPNT <tomnguyenhieu2004@gmail.com>
2025-08-09 23:00:04 +0700

    Update ApiResponse helper

--- app/Helpers/ApiResponse.php
+++ app/Helpers/ApiResponse.php
@@ -4,7 +4,25 @@ namespace App\Helpers;
 
 class ApiResponse
 {
-    public static function success($data = null, $message = 'Success.')
+    public static function continue()
+    {
+        return response()->json([
+            'success' => true,
+            'data' => null,
+            'message' => 'Request approved.'
+        ], 100);
+    }
+
+    public static function switchProtocol()
+    {
+        return response()->json([
+            'success' => true,
+            'data' => null,
+            'message' => 'Request switch protocol.'
+        ], 101);
+    }
+
+    public static function success($data = null, string $message = 'Success.')
     {
         return response()->json([
             'success' => true,
@@ -13,25 +31,27 @@ class ApiResponse
         ], 200);
     }
 
-    public static function dataNotfound($message = 'Data not found.')
+    public static function dataNotfound(array $errors = null, string $message = 'Data not found.')
     {
         return response()->json([
             'success' => true,
             'data' => null,
-            'message' => $message
+            'message' => $message,
+            'errors' => $errors
         ], 200);
     }
 
-    public static function internalServerError($message = 'Internal server error.')
+    public static function internalServerError(array $errors = null, string $message = 'Internal server error.')
     {
         return response()->json([
             'success' => false,
             'data' => null,
-            'message' => $message
+            'message' => $message,
+            'errors' => $errors
         ], 500);
     }
 
-    public static function unauthorized($message = 'Unauthorized.')
+    public static function unauthorized(string $message = 'Unauthorized.')
     {
         return response()->json([
             'success' => false,
@@ -40,7 +60,7 @@ class ApiResponse
         ], 401);
     }
 
-    public static function forbidden($message = 'Forbidden.')
+    public static function forbidden(string $message = 'Forbidden.')
     {
         return response()->json([
             'success' => false,
@@ -49,7 +69,7 @@ class ApiResponse
         ], 403);
     }
 
-    public static function unprocessableContent(array $errors = null, $message = 'Unprocessable Content.')
+    public static function unprocessableContent(array $errors = null, string $message = 'Unprocessable Content.')
     {
         return response()->json([
             'success' => false,
@@ -57,5 +77,24 @@ class ApiResponse
             'message' => $message,
             'errors' => $errors
         ], 422);
+    }
+
+    public static function badResource()
+    {
+        return response()->json([
+            'success' => false,
+            'data' => null,
+            'message' => 'Bad resource.'
+        ], 301);
+    }
+
+    public static function badRequest(array $errors = null, string $message = 'Bad request.')
+    {
+        return response()->json([
+            'success' => false,
+            'data' => null,
+            'message' => $message,
+            'errors' => $errors
+        ], 400);
     }
 }
